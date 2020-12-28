1fb3a8c3018e80e079829984b1a67d33c5aff76d
jkneko <jkneko@gmail.com>
2020-12-28 18:16:56 +0900

    init

---
+++ DataPortalPager/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DataPortalPager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DataPortalPager/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace DataPortalPager
+{
+    public partial class MainForm : Form
+    {
+        private CefSharp.WinForms.ChromiumWebBrowser webContent;
+        private int pageCount = 32;
+        private int currentPage = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Cursor.Hide();
+
+            // 初回のみ30秒とする
+            timer1.Interval = 30000;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            string appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Directory.SetCurrentDirectory(appPath);
+            CefSettings settings = new CefSettings();
+            settings.UserDataPath = appPath;
+            settings.CachePath = appPath + @"\caches";
+            settings.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66";
+            Cef.EnableHighDPISupport();
+            Cef.Initialize(settings);
+
+            webContent = new ChromiumWebBrowser("https://datastudio.google.com/embed/reporting/1h_UHdcPryyBwV2hLrW8G_eOhAsDA6ZB1/page/KG79");
+            this.Controls.Add(webContent);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 10000;
+            if (Form.ActiveForm != this)
+                return;
+
+            currentPage++;
+            if(currentPage >= pageCount)
+            {
+                SendKeys.Send("{HOME}");
+                currentPage = 0;
+            }
+            else
+            {
+                SendKeys.Send("{RIGHT}");
+            }
+        }
+    }
+}
